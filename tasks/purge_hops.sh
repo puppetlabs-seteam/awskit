d923bce08951a5366ba846ccd6f33061bb381429
Dimitri Tischenko <dimitri@puppet.com>
2018-02-23 17:42:11 +0100

    added purge_hops task

---
+++ tasks/purge_hops.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# purges all nodes with 'hops' in the name.
+
+$nodes = $(puppet cert list --all|grep hops|cut -d \" -f 2)
+if [ "$PT_force" == "true" ] ; then
+  puppet node purge $nodes
+else
+  echo "Nodes that would be purged:\n$nodes"
+fi
