8e722277bf6fd1b4902f1e10db35ee50f22f057c
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-03-14 11:17:14 +0100

    Update join_domain.pp

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,4 +1,7 @@
 class devhops::join_domain {
+  # this requires the following module to be set in the Puppetfile:
+  # mod 'trlinkin/domain_membership', '1.1.2'
+  # 
   class { 'domain_membership':
     domain       => 'devhops.local',
     username     => lookup('devhops::join_domain_user'),
