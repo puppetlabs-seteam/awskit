b140ff03f00f1f7a4b2ba2fd256cbcc977958675
Dimitri Tischenko <dimitri@puppet.com>
2018-03-29 11:02:19 +0100

    first version of provision script

--- tasks/provision.sh
+++ tasks/provision.sh
@@ -1,7 +1,41 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # Puppet Task Name: provision
 #
+
+usage() {
+
+cat << EOU
+
+Usage: 
+
+[_noop="yes"] [PT_count=<count>] PT_type=<type> $0
+
+or, as a shortcut: 
+
+[_noop="yes"] $0 <type> [<count>]
+
+- type should be one of: ['master', 'linux_node', 'windows_node', 'discovery', 'windc']
+- count should be an integer, default: value configured in hiera
+
+Examples:
+
+Provision a master:
+
+PT_type=master $0
+or
+$0 master
+h
+Provision 7 windows nodes in noop mode:
+
+_noop="yes" PT_count=7 PT_type=windows_node $0
+or
+_noop="yes" $0 windows_node 7
+
+EOU
+
+exit -2
+}
 
 if [ -z ${FACTER_user+x} ]; then FACTER_user=$USER; fi
 if [ -z ${FACTER_aws_region+x} ]; then FACTER_aws_region=$AWS_REGION; fi
@@ -13,10 +47,10 @@ if [ ! -z ${1+x} ]; then
   PT_type=$1
 else 
   if [ -z ${PT_type+x} ]; then 
-    echo "Please specify the type to provision"; exit -1; 
+    usage 
   fi
 fi
-
+if [ ! -z ${2+x} ]; then PT_count=$2; fi
 if [ ! -z ${_noop+x} ]; then echo "Noop mode requested"; noop="--noop"; fi
 
 case $PT_type in
@@ -25,12 +59,12 @@ case $PT_type in
   windows_node) ;;
   discovery) ;;
   windc) ;;
-  *)
-    echo "Type should be one of 'master', 'linux_node', 'windows_node', 'discovery', 'windc'"
-    exit -2
+  *) 
+    usage 
     ;;
 esac
 
+[[ $PT_count =~ "^[0-9]+$" ]] || { usage }
 
 class="devhops::create_$PT_type"
 
@@ -47,4 +81,4 @@ EOM
 echo "applying manifest:"
 echo "$manifest"
 
-echo $manifest | puppet apply --modulepath .. --noop
+echo $manifest | puppet apply --modulepath .. $noop
