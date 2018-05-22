6f6462d7f9b5f2a2accf4607c6f39e531b12a5d2
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 13:45:08 +0200

    Force git push as control repo overwrite is needed

--- tasks/conf_control_repo.sh
+++ tasks/conf_control_repo.sh
@@ -14,7 +14,7 @@ echo git clone $PT_control_repo control-repo
 git clone $PT_control_repo control-repo
 cd control-repo
 git remote add gogs git@localhost:puppet/control-repo.git
-git push gogs production
+git push gogs production --force
 if [ ! -z ${PT_public_key_name+x} ] && [ ! -z  ${PT_public_key_value+x} ] ; then
   url="http://localhost:3000/api/v1/user/keys"
   #echo curl -u puppet:puppetlabs -X POST -H "Content-Type: application/json" --data "$(gogs_params)" $url
