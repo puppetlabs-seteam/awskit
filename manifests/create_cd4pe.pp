a088f69b1bde7697150b3240d085e56a3d45fc8e
Dimitri Tischenko <dimitri@puppet.com>
2018-06-12 23:39:46 +0100

    added cd4pe creation class

---
+++ manifests/create_cd4pe.pp
@@ -0,0 +1,45 @@
+# awskit::create_cd4pe
+#
+# This class creates an instance in AWS for hosting a cd4pe docker host
+#
+# @summary Installs AWS instance for CD4PE installation
+#
+# @example
+#   include awskit::create_cd4pe
+class awskit::create_cd4pe (
+  $instance_type,
+  $user_data = lookup('awskit::create_linux_node::user_data'),
+  $instance_name = 'awskit-cd4pe',
+  $count = 1,
+) {
+
+  include awskit
+
+  $ami = $awskit::centos_ami
+
+  ec2_securitygroup { 'awskit-cd4pe':
+    ensure      => 'present',
+    region      => $awskit::region,
+    vpc         => $awskit::vpc,
+    description => 'Security group for CD4PE host',
+    ingress     => [
+      { protocol => 'tcp', port => 22,   cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 80,   cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 2222, cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 7000, cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 8000, cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 8080, cidr => '0.0.0.0/0', },
+      { protocol => 'tcp', port => 8081, cidr => '0.0.0.0/0', },
+      { protocol => 'icmp',              cidr => '0.0.0.0/0', },
+    ],
+  }
+
+  awskit::create_host { $instance_name:
+    ami             => $ami,
+    instance_type   => $instance_type,
+    user_data       => inline_epp($user_data),
+    security_groups => ['awskit-cd4pe'],
+    require         => Ec2_securitygroup['awskit-cd4pe'],
+  }
+
+}
