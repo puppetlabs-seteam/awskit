5dec8faa965579796124e735a42a14322fd4b860
Dimitri Tischenko <dimitri@puppet.com>
2018-06-27 12:50:48 +0200

    Removed facts and replaced by hiera fixtures

--- spec/classes/awskit_spec.rb
+++ spec/classes/awskit_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe 'awskit' do
-  let(:facts) do
-    {
-      'key_name'          => 'key_name',
-      'region'            => 'eu-west-2',
-      'availability_zone' => 'eu-west-2a',
-      'subnet'            => 'my_subnet',
-      'master_ip'         => '1.2.3.4',
-      'wsus_ip'           => '1.2.3.4',
-    }
-  end
-
   it { is_expected.to compile }
 end
