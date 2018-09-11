aeaeff4a34d837aefbe67bb074279f36cf07c049
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-09-11 15:26:06 +0200

    Update create_discovery_nodes.pp

--- manifests/create_discovery_nodes.pp
+++ manifests/create_discovery_nodes.pp
@@ -9,29 +9,18 @@
 class awskit::create_discovery_nodes (
   $instance_type,
   $user_data,
-  $count         = 1,
+  $count         = 10,
   $instance_name = 'awskit-disconode',
 ) {
 
   include awskit
-
-  ec2_securitygroup { $awskit::disconodes_sc_name:
-    ensure      => 'present',
-    region      => $awskit::region,
-    vpc         => $awskit::vpc,
-    description => 'ssh and http(s) ingress for awskit discovery',
-    ingress     => [
-      { protocol => 'tcp', port => 22,   cidr => '0.0.0.0/0', },
-      { protocol => 'icmp',              cidr => '0.0.0.0/0', },
-    ],
-  }
 
   range(1,$count).each | $i | {
     awskit::create_host { "${instance_name}-${i}":
       ami             => $awskit::centos_ami,
       instance_type   => $instance_type,
       user_data       => $user_data,
-      security_groups => [$awskit::disconodes_sc_name],
+      security_groups => [$awskit::disco_sc_name],
     }
   }
 }
