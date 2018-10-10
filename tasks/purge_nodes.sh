4fe9c2e1205a9d26b796f540bc066d1a5f72771b
Dimitri Tischenko <dimitri@puppet.com>
2018-10-10 02:02:21 +0200

    ca command updated to PE2019

--- tasks/purge_nodes.sh
+++ tasks/purge_nodes.sh
@@ -4,7 +4,7 @@
 # FIXME: completely untested
 # FIXME: the puppetmaster needs to be excluded from the purge
 
-nodes=$(puppet cert list --all|grep ${PT_pattern} | grep -v master |cut -d \" -f 2)
+nodes=$(puppetserver ca list --all|grep ${PT_pattern} | grep -v master |cut -d \" -f 2)
 if [ "$PT_force" == "true" ] ; then
   puppet node purge $nodes
 else
