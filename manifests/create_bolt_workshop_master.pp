ff6c2d1f146d4fd6877b7ffe5667bffeab37ce4e
Dimitri Tischenko <dimitri@puppet.com>
2018-12-28 11:40:29 +0100

    Removed all userdata templates from hiera and moved to files or inline templates

--- manifests/create_bolt_workshop_master.pp
+++ manifests/create_bolt_workshop_master.pp
@@ -34,7 +34,6 @@ class awskit::create_bolt_workshop_master (
   awskit::create_host { $instance_name:
     ami             => $awskit::centos_ami,
     instance_type   => $instance_type,
-    user_data       => '',
     security_groups => ["${facts['user']}-awskit-boltws"]
   }
 
